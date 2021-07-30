ed58c123745b241a6f071997a7bb1a8de2a36b0c
Daniel Seldura <daniel.seldura@gmail.com>
2021-07-30 18:48:58 +0800

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -22,137 +22,35 @@ class MyApp extends StatelessWidget {
 
 class MapboxNavigationTutorial extends StatefulWidget {
   @override
-  _MapboxNavigationTutorialState createState() =>
-      _MapboxNavigationTutorialState();
+  _MapboxNavigationTutorialState createState() => _MapboxNavigationTutorialState();
 }
 
 class _MapboxNavigationTutorialState extends State<MapboxNavigationTutorial> {
-  late MapBoxNavigation _directions;
-  late MapBoxNavigationViewController _controller;
-
-  var _isMultipleStop;
-  @override
-  void initState() {
-    super.initState();
-    _directions = MapBoxNavigation(onRouteEvent: _onRouteEvent);
-  }
-
-  _onRouteEvent(e) async {
-    // FutureOr<double> _distanceRemaining = await _directions.distanceRemaining;
-    //FutureOr<double> _durationRemaining = await _directions.durationRemaining;
-
-    // print('distanceRemaining: $_distanceRemaining');
-    //print('durationRemaining: $_durationRemaining');
-
-    switch (e.eventType) {
-      case MapBoxEvent.progress_change:
-        var progressEvent = e.data as RouteProgressEvent;
-        var _arrived = progressEvent.arrived;
-        if (progressEvent.currentStepInstruction != null)
-          var _instruction = progressEvent.currentStepInstruction;
-        break;
-      case MapBoxEvent.route_building:
-      case MapBoxEvent.route_built:
-        var _routeBuilt = true;
-        break;
-      case MapBoxEvent.route_build_failed:
-        var _routeBuilt = false;
-        break;
-      case MapBoxEvent.navigation_running:
-        var _isNavigating = true;
-        break;
-      case MapBoxEvent.on_arrival:
-        var _arrived = true;
-        if (!_isMultipleStop!) {
-          await Future.delayed(Duration(seconds: 3));
-          await _controller.finishNavigation();
-        } else {}
-        break;
-      case MapBoxEvent.navigation_finished:
-      case MapBoxEvent.navigation_cancelled:
-        var _routeBuilt = false;
-        var _isNavigating = false;
-        break;
-      default:
-        break;
-    }
-    //refresh UI
-    setState(() {});
-  }
-
-  var _options = MapBoxOptions(
-      initialLatitude: 10.282204,
-      initialLongitude: 123.881575,
-      zoom: 14.0,
-      tilt: 0.0,
-      bearing: 0.0,
-      enableRefresh: true,
-      alternatives: true,
-      voiceInstructionsEnabled: true,
-      bannerInstructionsEnabled: true,
-      allowsUTurnAtWayPoints: true,
-      mode: MapBoxNavigationMode.driving,
-      units: VoiceUnits.imperial,
-      simulateRoute: true,
-      language: "en");
-
-  mapboxNavigation() async {
-    Position currentPosition = await Geolocator.getCurrentPosition();
-
-    print('my current position: $currentPosition');
-    print('currentPosition: $currentPosition');
-    final cityhall = WayPoint(
-        name: "City Hall",
-        latitude: currentPosition.latitude,
-        longitude: currentPosition.longitude);
-    final downtown = WayPoint(
-        name: "Sm Seaside", latitude: 10.282204, longitude: 123.881575);
-
-    var wayPoints = <WayPoint>[];
-    wayPoints.add(cityhall);
-    wayPoints.add(downtown);
-    _controller.buildRoute(wayPoints: wayPoints);
-    print('wayPoints: $wayPoints');
-
-    await _directions.startNavigation(wayPoints: wayPoints, options: _options);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
-      Container(
-          color: Colors.grey,
-          child: MapBoxNavigationView(
-              options: _options,
-              onRouteEvent: _onRouteEvent,
-              onCreated: (MapBoxNavigationViewController controller) async {
-                _controller = controller;
-              })),
+      // Container(
+      //     color: Colors.grey,
+      //     child: MapBoxNavigationView(
+      //         options: _options,
+      //         onRouteEvent: _onRouteEvent,
+      //         onCreated: (MapBoxNavigationViewController controller) async {
+      //           _controller = controller;
+      //         })),
       ElevatedButton(
         onPressed: () async {
           print('I AM PRESSED');
-          mapboxNavigation();
-          // _controller.startNavigation();
-          // await DriverNavigationService(
-          //         destination: Position(
-          //             timestamp: DateTime.now(),
-          //             longitude: 10.282204,
-          //             latitude: 123.881575,
-          //             accuracy: 10,
-          //             altitude: 9,
-          //             heading: 10,
-          //             speed: 10,
-          //             speedAccuracy: 10,
-          //             floor: 1,
-          //             isMocked: true))
-          //     .navigateViaMapBox();
+          await DriverNavigationService(
+              destination: Position.fromMap({
+            "latitude": 10.29349592073334,
+            "longitude": 123.901997407398,
+          })).navigateViaMapBox();
         },
         child: Text('hi'),
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.resolveWith<Color>(
             (Set<MaterialState> states) {
-              if (states.contains(MaterialState.pressed))
-                return Theme.of(context).colorScheme.primary;
+              if (states.contains(MaterialState.pressed)) return Theme.of(context).colorScheme.primary;
               // Use the component's default.
               return Theme.of(context).colorScheme.secondary.withOpacity(0.5);
             },
@@ -160,5 +58,53 @@ class _MapboxNavigationTutorialState extends State<MapboxNavigationTutorial> {
         ),
       ),
     ]);
+  }
+}
+
+class DriverNavigationService {
+  MapBoxNavigation _directions;
+  Position destination;
+  DriverNavigationService({required this.destination})
+      : _directions = MapBoxNavigation(onRouteEvent: routeEventHandler);
+
+  navigateViaMapBox() async {
+    Position currentPosition = await Geolocator.getCurrentPosition(forceAndroidLocationManager: true);
+    MapBoxOptions _mapBoxOptions = MapBoxOptions(
+        voiceInstructionsEnabled: true,
+        bannerInstructionsEnabled: true,
+        longPressDestinationEnabled: false,
+        mode: MapBoxNavigationMode.driving,
+        simulateRoute: false,
+        tilt: 0.0,
+        bearing: 0.0,
+        language: "en",
+        units: VoiceUnits.metric,
+        zoom: 23,
+        animateBuildRoute: false,
+        initialLatitude: currentPosition.latitude,
+        initialLongitude: currentPosition.longitude,
+        enableRefresh: true);
+    List<WayPoint> wayPoints = [];
+    wayPoints.add(WayPoint(name: "Start", latitude: currentPosition.latitude, longitude: currentPosition.longitude));
+    wayPoints.add(WayPoint(name: "Destination", latitude: destination.latitude, longitude: destination.longitude));
+    return await _directions.startNavigation(wayPoints: wayPoints, options: _mapBoxOptions);
+  }
+
+  static routeEventHandler(RouteEvent e) async {
+    print(e.eventType);
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+    switch (e.eventType) {
+      case MapBoxEvent.navigation_running:
+        print("Navigation has started");
+        break;
+      case MapBoxEvent.milestone_event:
+        break;
+      case MapBoxEvent.navigation_finished:
+      case MapBoxEvent.navigation_cancelled:
+        print("Navigation has ended");
+        break;
+      default:
+        break;
+    }
   }
 }
